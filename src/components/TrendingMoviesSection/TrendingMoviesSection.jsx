6275a250ca8f7f6e77f79546d34fea20d0309485
Rosen Yordanov <roskata182003@gmail.com>
2024-04-11 15:23:25 +0300

    Finish TrendingMoviesSection component

--- src/components/TrendingMoviesSection/TrendingMoviesSection.jsx
+++ src/components/TrendingMoviesSection/TrendingMoviesSection.jsx
@@ -1,6 +1,9 @@
 import "./TrendingMoviesSection.css";
 import {loadPopularMovies} from "../../services/movieService";
 import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SlidingMovieCard from "../SlidingMovieCard/SlidingMovieCard";
+import { Autoplay} from 'swiper/modules';
 
 export default function TrendingMoviesSection(){
   const [movies, setMovies] = useState([]);
@@ -8,9 +11,25 @@ export default function TrendingMoviesSection(){
    loadPopularMovies(1)
    .then(res => setMovies(res.results))
   },[])
+  const movieItems = movies.map((m, index) => {
+     return <SwiperSlide key={index}>
+         <SlidingMovieCard movie={m}/>
+     </SwiperSlide>
+  })
   return(
     <section className="trending-movies">
       <h1 className="trending-movies-title">Trending Movies</h1>
+      <Swiper
+         grabCursor={true}
+         spaceBetween={10}
+         slidesPerView={'auto'}
+         autoplay={{
+          delay: 1000,
+          paused: true
+        }}
+        modules={[Autoplay]}>
+        {movieItems}
+      </Swiper>
     </section>  
   )
 }
