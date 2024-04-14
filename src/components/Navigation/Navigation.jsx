d6647d026299c57daaf715cf296dd48071f215f7
Rosen Yordanov <roskata182003@gmail.com>
2024-04-14 10:10:24 +0300

    Add responsive design on NavigationComponent

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import "../Navigation/Navigation.css";
+import "../Navigation/NavigationResponsive.css";
 import { useState } from "react";
 
 export default function Navigation(){
     const [isActive, setIsActive] = useState(false);
+    const [isActiveButton, setIsActiveButton] = useState(false);
 
     window.addEventListener("scroll", function(){
         if(this.window.scrollY > 0){
@@ -17,6 +19,11 @@ export default function Navigation(){
     })
     return(
         <header className={`${isActive ? 'active-header' : ''}`}>
+        <div onClick={() => setIsActiveButton(!isActiveButton)} className={`menu-icon ${isActiveButton ? 'closed' : ''}`}>
+            <div className="first-bar menu-icon-bar"></div>
+            <div className="second-bar menu-icon-bar"></div>
+            <div className="third-bar menu-icon-bar"></div>
+        </div>
              <div className="header-logo">
                  <h3 className="header-logo-title">Movies For You</h3>    
                  <i className="fa-solid fa-video"></i>
